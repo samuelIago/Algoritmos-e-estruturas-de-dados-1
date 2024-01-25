0bb9a6ddd82082279e3e93baa17ae780a95a1971
[Samuel Iago] <samueliagocabral@gmail.com>
2024-01-25 17:18:22 -0300

    refac matricula,lanca_notas

--- Tipos-estruturados/exercicio2.c
+++ Tipos-estruturados/exercicio2.c
@@ -4,93 +4,95 @@ b) Implemente uma função que lança as notas e calcula a média dos alunos da 
 c) Implemente uma função que imprime os dados de todos os alunos da disciplina. Protótipo: void imprime_tudo(int n, Aluno** alunos);
 d) Implemente uma função que imprime os dados de todos os alunos de uma turma. Protótipo: void imprime_turma(int n, Aluno** alunos, char turma);
 e) Implemente uma função que imprime os alunos aprovados de uma determinada turma. Para ser aprovado, o aluno deve ter média maior que ou igual a 7,0. Protótipo: void imprime_turma_aprovados(int n, Aluno**alunos, char turma);
-                            Sugestões: 
-i) Use um vetor de ponteiros para estrutura; 
+                            Sugestões:
+i) Use um vetor de ponteiros para estrutura;
 ii) Use uma constante simbólica para representar o número
-máximo de alunos. 
+máximo de alunos.
 Iii) Antes de manipular o vetor de ponteiros, inicialize cada um de seus elementos com NULL
 (você pode escrever uma função para isso).
 OBS.: Os exercícios apresentados sugerem a implementação de diferentes funções. Sendo assim, o programador
 deve escrever um programa (função main) para testar sua implementação.
 */
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<locale.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
 
 #define maxAlunos 3
 
-typedef struct aluno{
+typedef struct aluno
+{
     int matricula;
     char nome[80];
     char turma;
     float notas[3];
     float media;
-}Aluno;
-//eu testei como se as turmas fosse A, B, C, etc
+} Aluno;
+// eu testei como se as turmas fosse A, B, C, etc
 
-void matricula(int n, Aluno **alunos){
-    if(n > 0){
-        int i;
-        *alunos =(Aluno*) malloc(sizeof(Aluno));
-        printf("Essa disciplina tem %d vagas disponiveis. \n", n);
-        printf("Insira o seu nome:\n");
-        scanf(" %[^\n]",(*alunos)->nome);
-        printf("Insira o seu numero de matricula: \n");
-        scanf("%d",&(*alunos)->matricula);
-        printf("Insira a sua turma: \n");
-        scanf(" %c",&(*alunos)->turma);
-        printf("Insira as suas 3 notas: \n");
-        for(i = 0; i < 3; i++){
-            printf("Nota: %d\n", i+1);
-        scanf(" %f",&(*alunos)->notas[i]);        
-        
-        }
-    }else{
-        printf("Essa disciplina esta lotada.\n");
-
+void matricula(int n, Aluno **alunos)
+{
+    int i;
+    alunos[n] = (Aluno *)malloc(sizeof(Aluno));
+    printf("Cadastre o aluno: %d \n", n+1);
+    printf("Insira o seu nome:\n");
+    scanf(" %[^\n]", (alunos)[n]->nome);
+    printf("Insira o seu numero de matricula: \n");
+    scanf("%d", &(alunos)[n]->matricula);
+    printf("Insira a sua turma: \n");
+    scanf(" %c", &(alunos)[n]->turma);
+    printf("Insira as suas 3 notas: \n");
+    for (i = 0; i < 3; i++){
+        printf("Nota: %d\n", i + 1);
+        scanf(" %f", &(alunos)[n]->notas[i]);
     }
 }
+
 void lanca_notas(int n, Aluno **alunos)
 {
-    int i = 0,j = 0;
+    int i = 0, j = 0;
     float somaNotas = 0;
     for (i = 0; i < n; i++){
         for (j = 0; j < 3; j++){
             somaNotas += (*alunos[i]).notas[j];
-            
         }
-            (*alunos[i]).media = somaNotas / 3.0;
+        (*alunos[i]).media = somaNotas / 3.0;
         printf("Media do aluno: \n");
-        printf("%.2f\n",(*alunos[i]).media);
-        }
+        printf("%.2f\n", (*alunos[i]).media);
         somaNotas = 0;
+    }
+    
 }
 
 /*
 void imprime_tudo(int n, Aluno **alunos);
 void imprime_turma(int n, Aluno **alunos, char turma);
 void imprime_turma_aprovados(int n, Aluno **alunos, char turma);*/
-int main(){
+int main()
+{
     int alunosMat;
     int i;
     printf("Insira quantos alunos serao matriculados:\n");
-    scanf("%d",&alunosMat);
+    scanf("%d", &alunosMat);
 
-    Aluno **estudante = (Aluno**) malloc(alunosMat * sizeof(Aluno*));
-    if (alunosMat<=maxAlunos){
+    Aluno **estudante = (Aluno **)malloc(alunosMat * sizeof(Aluno *));
+    if (alunosMat <= maxAlunos)
+    {
         for (i = 0; i < alunosMat; i++){
-            matricula(alunosMat,&estudante[i]);
+            matricula(i, estudante);
+            lanca_notas(alunosMat, estudante);//aqui tem que ser alunosMat
         }
-    }else{
-            printf("limite de estudantes ultrapassado.");
-        }
-        lanca_notas(alunosMat, &estudante[i]);
-            // assosiar a variavel vagas ao tamanho do vetor
-        for (i = 0; i < alunosMat; i++)
-        {
-            free(estudante[i]);
-        }
-        free(estudante);
-        return 0;
     }
+    else
+    {
+        printf("limite de estudantes ultrapassado.");
+    }
+    
+    // assosiar a variavel vagas ao tamanho do vetor
+    for (i = 0; i < alunosMat; i++)
+    {
+        free(estudante[i]);
+    }
+    free(estudante);
+    return 0;
+}
